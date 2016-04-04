f95c50356a606b016ef7d106ebf027955f842add
Roberth Siqueira <roberth.siqueira@synca.com.br>
2016-04-04 02:40:53 -0300

    Initial Commit

---
+++ gohotmart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gohotmart
+//
+//  Created by Roberth Siqueira on 04/04/16.
+//  Copyright © 2016 Roberth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gohotmart/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gohotmart
+//
+//  Created by Roberth Siqueira on 04/04/16.
+//  Copyright © 2016 Roberth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
